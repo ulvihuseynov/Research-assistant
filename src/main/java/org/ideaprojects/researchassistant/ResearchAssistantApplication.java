d20c64428b24e29325709bb0c44fbc249f4456c8
ulvihuseynov <ulvihuseynov441@gmail.com>
2025-11-13 14:43:44 +0400

    Initial commit

---
+++ src/main/java/org/ideaprojects/researchassistant/ResearchAssistantApplication.java
@@ -0,0 +1,13 @@
+package org.ideaprojects.researchassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ideaprojects/researchassistant/ResearchAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ideaprojects.researchassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
